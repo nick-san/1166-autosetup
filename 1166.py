3d7a411c2a320a7e3bdaeba47790324f94a77bb1
Nick <hirohiro239@outlook.jp>
2024-02-21 19:33:52 +0900

    second commit

--- 1166.py
+++ 1166.py
@@ -9,6 +9,9 @@ import time
 print("WSR-1166DHPL2(v2) 2.03 インストール & 自動設定スクリプト")
 print("> adminのパスワードを入力してEnterキーを押下してください。")
 password = input()
+print("> ローカルの1166のファームウェアのフルパスを入力してEnterキーを押下してください。")
+#firmware_file_path = "/home/nick/Downloads/wsr1166dhpl2v2-203/wsr-1166dhpl2v2-203"
+firmware_file_path = input()
 
 # WebDriverの初期化（Chromeの場合）
 driver = webdriver.Chrome()
@@ -60,7 +63,7 @@ driver.find_element(By.ID, "AT_ADMIN").click()
 driver.find_element(By.ID, "sub_meu6_3").click()
 time.sleep(3)
 driver.switch_to.frame("content_main")
-firmware_file_path = "/home/nick/Downloads/wsr1166dhpl2v2-203/wsr-1166dhpl2v2-203"
+#firmware_file_path = "/home/nick/Downloads/wsr1166dhpl2v2-203/wsr-1166dhpl2v2-203"
 time.sleep(5)
 driver.find_element(By.NAME, "file").send_keys(firmware_file_path)
 time.sleep(10)
