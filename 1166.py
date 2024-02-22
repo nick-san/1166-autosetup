bd9876a1a3815baf1cb6eb3cf4ab590242daafaf
Nick <45539267+nick-san@users.noreply.github.com>
2024-02-22 18:04:26 +0900

    Update 1166.py

--- 1166.py
+++ 1166.py
@@ -1,26 +1,34 @@
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import time
 
 print("WSR-1166DHPL2(v2) 2.03 インストール & 自動設定スクリプト")
 print("> adminのパスワードを入力してEnterキーを押下してください。")
 password = input()
 print("> ローカルの1166のファームウェアのフルパスを入力してEnterキーを押下してください。")
-#firmware_file_path = "/home/nick/Downloads/wsr1166dhpl2v2-203/wsr-1166dhpl2v2-203"
+print("> (何も入力せずにEnter押下でデフォルト値が使用されます。)")
+print("> (デフォルト値: C:\\BUFFALO\\wsr1166dhpl2v2-203\\wsr-1166dhpl2v2-203)")
 firmware_file_path = input()
+if firmware_file_path == "":
+    firmware_file_path = "C:\\BUFFALO\\wsr1166dhpl2v2-203\\wsr-1166dhpl2v2-203"
 
 # WebDriverの初期化（Chromeの場合）
-driver = webdriver.Chrome()
+# driver_path = "C:/Users/yuki/AppData/Local/Google/Chrome/Application/chromedriver"
+driver_path = "C:\\chromedriver-win64\\chromedriver.exe"
+driver = webdriver.Chrome(service=ChromeService(driver_path))
+# driver = webdriver.Chrome()
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 # ルーターのIPアドレス
 router_ip = "http://192.168.11.1"
 
 # ログインページにアクセス
 driver.get(router_ip)
+driver.maximize_window()
 
 # ログイン情報
 #password = "4hbkmx3b"
@@ -35,6 +43,7 @@ driver.find_element(By.ID, "id_login").click()
 driver.get("http://192.168.11.1/index_adv.html")
 
 # DHCPサーバーからの自動取得に変更
+time.sleep(3)
 driver.switch_to.default_content()
 driver.find_element(By.ID, "AT_WAN").click()
 driver.find_element(By.ID, "sub_meu1_0").click()
@@ -63,7 +72,7 @@ driver.find_element(By.ID, "AT_ADMIN").click()
 driver.find_element(By.ID, "sub_meu6_3").click()
 time.sleep(3)
 driver.switch_to.frame("content_main")
-#firmware_file_path = "/home/nick/Downloads/wsr1166dhpl2v2-203/wsr-1166dhpl2v2-203"
+#firmware_file_path = "/path/to/firmware"
 time.sleep(5)
 driver.find_element(By.NAME, "file").send_keys(firmware_file_path)
 time.sleep(10)
